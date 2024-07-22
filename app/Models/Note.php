97fba90f0ae56e4a10a25dcfc452c5d8b717eeec
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-07-22 15:52:30 +0330

    feat (model) : add the relation between note and user in the note

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Note extends Model
 {
@@ -13,5 +14,10 @@ class Note extends Model
     protected $guarded = [
         'id',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 
 }
