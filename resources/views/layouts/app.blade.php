ec2ffb87cd8f6801855962e8b4ff80df5ac1ed42
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-09-16 10:37:39 +0330

    install the alpain.js and add the tag in the app layout as the need the wire.ui

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,9 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         <wireui:scripts />
+        <script src="//unpkg.com/alpinejs" defer></script>
+        
+        <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.14.1/dist/cdn.min.js"></script>
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
