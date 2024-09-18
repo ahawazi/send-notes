0557004ba8c4b2c3220141681d086a21341baeeb
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-09-18 20:14:52 +0330

    fead : add dashboardstats

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,11 +7,7 @@
 
     <div class="py-12">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <x-button rose>hi there</x-button>
-                </div>
-            </div>
+            <livewire:dashboardstats>
         </div>
     </div>
 </x-app-layout>
